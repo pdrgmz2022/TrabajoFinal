50dd86f74f4a3e173ce8d71f4284ce8732e01226
unknown <pdrgmz2022@hgmail.com>
2022-03-29 00:40:15 -0300

    primera subida

---
+++ archivos/script.js
@@ -0,0 +1,27 @@
+
+/* video */ 
+let video=document.querySelector('video');
+let playBoton = document.getElementById("play");
+let pauseBoton= document.getElementById("pause");
+
+
+
+
+document.getElementById('showTime').innerHTML=` Duracion video : ${video.duration.toFixed(2)}`;
+
+for (let i = 0; i < video.tim; i++) {
+    console.log(video.currentTime)
+    
+}
+
+playBoton.addEventListener('click',()=>{
+    video.play()
+
+   
+});
+
+pauseBoton.addEventListener('click',()=>{
+    video.pause();
+    flag=false;
+});
+
